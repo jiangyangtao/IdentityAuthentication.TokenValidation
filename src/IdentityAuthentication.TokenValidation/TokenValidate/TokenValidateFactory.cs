ae72d768a7f8768c6b02061eba8900d79b58fa5f
Yangtao Jiang <jiangyangtao1@qq.com>
2023-09-25 02:39:42 +0800

    Fixed bugs

--- src/IdentityAuthentication.TokenValidation/TokenValidate/TokenValidateFactory.cs
+++ src/IdentityAuthentication.TokenValidation/TokenValidate/TokenValidateFactory.cs
@@ -1,4 +1,5 @@
 ﻿using IdentityAuthentication.TokenValidation.Abstractions;
+using IdentityAuthentication.TokenValidation.Enums;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace IdentityAuthentication.TokenValidation.TokenValidate
@@ -6,16 +7,18 @@ namespace IdentityAuthentication.TokenValidation.TokenValidate
     internal class TokenValidateFactory : ITokenValidateFactory
     {
         private readonly IServiceProvider _serviceProvider;
+        private readonly IAuthenticationConfigurationProvider _configurationProvider;
 
-        public TokenValidateFactory(IServiceProvider serviceProvider)
+        public TokenValidateFactory(IServiceProvider serviceProvider, IAuthenticationConfigurationProvider configurationProvider)
         {
             _serviceProvider = serviceProvider;
+            _configurationProvider = configurationProvider;
         }
 
         public ITokenValidateProvider CreateTokenValidateProvider()
         {
             var connectionType = ConnectionType.Http;
-            if (TokenValidationConfiguration.AuthenticationConfiguration.EnableGrpcConnection) connectionType = ConnectionType.Grpc;
+            if (_configurationProvider.AuthenticationConfiguration.EnableGrpcConnection) connectionType = ConnectionType.Grpc;
 
             return _serviceProvider.GetServices<ITokenValidateProvider>().FirstOrDefault(a => a.ConnectionType == connectionType);
         }
