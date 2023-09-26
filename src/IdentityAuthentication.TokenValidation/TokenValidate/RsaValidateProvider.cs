5ef75e9a8a67869395883ba8d7a0b0eed6c901b1
Yangtao Jiang <jiangyangtao1@qq.com>
2023-09-27 02:01:19 +0800

    Add RsaValidateProvider

---
+++ src/IdentityAuthentication.TokenValidation/TokenValidate/RsaValidateProvider.cs
@@ -0,0 +1,16 @@
+﻿using IdentityAuthentication.TokenValidation.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityAuthentication.TokenValidation.TokenValidate
+{
+    internal class RsaValidateProvider : ITokenValidateProvider
+    {
+        public RsaValidateProvider()
+        {
+        }
+    }
+}
