a101ee8510e90c8c600c385d5b7d76e71f655db8
Yangtao Jiang <jiangyangtao1@qq.com>
2023-09-28 03:06:06 +0800

    Fixed bugs

--- src/IdentityAuthentication.TokenValidation/RsaValidateProvider.cs
+++ src/IdentityAuthentication.TokenValidation/RsaValidateProvider.cs
@@ -7,29 +7,29 @@ namespace IdentityAuthentication.TokenValidation
 {
     internal class RsaValidateProvider : ITokenValidateProvider
     {
-        private readonly JwtSecurityTokenHandler _jwtSecurityTokenHandler;
-        private readonly Model.TokenValidation _tokenValidation;
-        private readonly TokenValidationParameters _tokenValidationParameters;
         private readonly IRefreshTokenProvider _refreshTokenService;
         private readonly Credentials PublicCredentials;
+        private readonly IAuthenticationConfigurationProvider _configurationProvider;
+        private readonly JwtSecurityTokenHandler _jwtSecurityTokenHandler;
 
         public RsaValidateProvider(IRefreshTokenProvider refreshTokenService, IAuthenticationConfigurationProvider configurationProvider)
         {
             var publicSignature = configurationProvider.RsaVerifySignatureConfiguration.ToRsaSignature();
             PublicCredentials = new Credentials(publicSignature);
 
-            _tokenValidation = new Model.TokenValidation(configurationProvider.AccessTokenConfiguration, PublicCredentials);
-
             _jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
-            _tokenValidationParameters = _tokenValidation.GenerateTokenValidation();
             _refreshTokenService = refreshTokenService;
+            _configurationProvider = configurationProvider;
         }
 
         public bool IsRsaValidate => true;
 
         public async Task<TokenValidationResult> TokenValidateAsync(string token)
         {
-            var tokenValidationResult = await _jwtSecurityTokenHandler.ValidateTokenAsync(token, _tokenValidationParameters);
+            using var tokenValidation = new Model.TokenValidation(_configurationProvider.AccessTokenConfiguration, PublicCredentials);
+
+            var tokenValidationParameters = tokenValidation.GenerateTokenValidation();
+            var tokenValidationResult = await _jwtSecurityTokenHandler.ValidateTokenAsync(token, tokenValidationParameters);
             if (tokenValidationResult.IsValid) await _refreshTokenService.RefreshTokenAsync(tokenValidationResult.ClaimsIdentity.Claims);
 
             return tokenValidationResult;
